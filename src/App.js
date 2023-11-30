db9873e08f412c3bc3fea4e446368990efe44913
Chuchundra009 <redfox161294@gmail.com>
2023-11-30 14:29:55 +0300

    add homework one

--- src/App.js
+++ src/App.js
@@ -1,24 +1,32 @@
-import logo from './logo.svg';
 import './App.css';
+import Chart from 'react-google-charts';
 
-function App() {
+
+export const data = [
+  ["Task", "Hours per Day"],
+  ["Проснуться, размяться", 1],
+  ["Завтрак", 1],
+  ["Катание в горах", 6],
+  ["Обед", 1],
+  ["Работа за компом", 3],
+  ["Ужин", 1],
+  ["Игра в настолку", 3],
+  ["Сон", 8],
+];
+
+export const options = {
+  title: "Мой день в горах зимой",
+};
+
+export function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Chart
+      chartType="PieChart"
+      data={data}
+      options={options}
+      width={"100%"}
+      height={"400px"}
+    />
   );
 }
 
